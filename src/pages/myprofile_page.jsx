178c50ebb5137ee2a95cac29e5f69708045ee651
zookee76 <135536455+zookee76@users.noreply.github.com>
2024-02-10 21:17:08 +0800

    user profile

--- src/pages/myprofile_page.jsx
+++ src/pages/myprofile_page.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import './myprofile_page.css';
 
 function myprofile_page() {
     return (
-        <div>
-                <header>
-                    <h1>Hi I'm Nigga</h1>
-                </header>
+        <div className="page">
+            <div className="profile_container">
+                <img src="images/Screenshot_15.png" class="profile_banner" alt="banner"/>
+                <img src="images/Screenshot_16.png" class="profile_avatar avatar" alt="avatar"/>
+                <div className="profile_name">
+                    Yco Santos
+                </div>
+                <div className="profile_bio">
+                    I am CCS ID 122 thats dating a 17 year old minor
+                </div>
+
+                <button className="button edit_profile">
+                    Edit Profile
+                </button>
+
+                <button className="button more_profile">
+                    ...
+                </button>
+            </div>
+            
         </div>
     );
 }
