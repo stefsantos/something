31af155a36a8c6d00fe9c4f8b48de71173886a9d
Phrezie <philippmatty@gmail.com>
2024-02-12 22:52:18 +0800

    made a share popup

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -3,11 +3,14 @@ import './HomePage.css';
 import { Link } from 'react-router-dom'
 import './Post.css';
 import CommentPopup from './CommentPopup';
+import SharePopup from './SharePopup';
 
 function Post({ post }) {
     
     const [isHeartActive, setIsHeartActive] = useState(false);
     const [showComments, setShowComments] = useState(false);
+    const [showShare, setShowShare] = useState(false);
+    
     // Event handler for clicking the heart icon
     const toggleHeart = () => {
         setIsHeartActive(!isHeartActive);
@@ -22,6 +25,14 @@ function Post({ post }) {
     const closeComments = () => {
         setShowComments(false);
     }
+
+    const openShare = () => {
+        setShowShare(true);
+    };
+
+    const closeShare = () => {
+        setShowShare(false);
+    };
     
     return (
         <div className = "post">
@@ -40,12 +51,13 @@ function Post({ post }) {
                 <img src={post.imageUrl} alt="Post visual" className="post-image" />
             )}
             <div className='postactions'>
-            <div className={`heart ${isHeartActive ? 'heart-active' : ''}`} onClick={toggleHeart}></div>
-            <div className='comment' onClick={openComments}></div>
-                <div className='share'></div>
+                <div className={`heart ${isHeartActive ? 'heart-active' : ''}`} onClick={toggleHeart}></div>
+                <div className='comment' onClick={openComments}></div>
+                <div className='share' onClick={openShare}></div>
             </div>
             <small>{post.timestamp}</small>
-            <CommentPopup isOpen={showComments} onClose={closeComments} post={post} />    
+            <CommentPopup isOpen={showComments} onClose={closeComments} post={post} />
+            <SharePopup isOpen={showShare} onClose={closeShare} post={post} />
         </div>
 
         
