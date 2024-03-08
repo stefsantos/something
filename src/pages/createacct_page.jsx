503b664cae436cd04222804669721d66d0b7e244
Sn3s <ycosantos04@gmail.com>
2024-03-08 19:13:32 +0800

    connected frontend to backend (sign up and login)

--- src/pages/createacct_page.jsx
+++ src/pages/createacct_page.jsx
@@ -2,102 +2,166 @@ import React, { useState, useLayoutEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './createacct_page.css';
 import logo from '../assets/CineShare Logo Request.webp';
+import { useToast } from "@chakra-ui/react";
+
 
 function CreateAcctPage({ setShowNavbar }) {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const [userName, setUserName] = useState('');
-  const [newEmail, setNewEmail] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
-  const navigateHome = () => {
-    navigate('/home_page');
-    setShowNavbar(true);
-  }
+    const toast = useToast();
 
-  const handleSignup = (event) => {
-    event.preventDefault();
-    console.log('Signup with:', newEmail, newPassword);
-    navigateHome();
-  };
+    const navigateSignIn = () => {
+        navigate('/signin_page');
+        setShowNavbar(true);
+    };
 
+    const [inputs, setInputs] = useState ({
+        username: "",
+        email: "",
+        password: "",
+    });
 
-  const navigateSignIn = () => {
-    navigate('/signin_page');
-    setShowNavbar(true);
-  };
+    const handleSignUp = async (event) => {
+        event.preventDefault();
+        
+        if (inputs.password !== confirmPassword) {
+            toast({
+                title: "Error",
+                description: "Passwords do not match",
+                status: "error",
+                duration: 3000,
+                isClosable: true,
+            });
+            return; // Stop the function if passwords do not match
+        }
+        
+        try {
+            const res = await fetch("/api/users/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(inputs)
+            })
 
-  useLayoutEffect(() => {
-    setShowNavbar(false);
-  }, []);
+            if (!res.ok) {
+                // If the HTTP status code is not successful, handle it
+                throw new Error('Server responded with a status: ' + res.status);
+            }
+            
+            let data;
+        try {
+            data = await res.json();
+        } catch (jsonSyntaxError) {
+            throw new Error('Server response was not valid JSON');
+    }
 
-  return (
-    <div className="create-account-fullscreen">
-      <div className="create-account-fullscreen">
-          <div className="create-account-side-bar">
-              <img src={logo} alt="logo" height="400px" width="400px" />
-          </div>
-          <div className="create-account-container">
-              <div className="signup-container">
-                  <h2>Create Account</h2>
-                  <form className="signup-form" onSubmit={handleSignup}>
-                      <div className="input-group">
-                          <label htmlFor="userName">Username</label>
-                          <input
-                              id="userName"
-                              type="text"
-                              value={userName}
-                              onChange={(e) => setUserName(e.target.value)}
-                              required
-                          />
-                      </div>
-                      <div className="input-group">
-                          <label htmlFor="newEmail">Email</label>
-                          <input
-                              id="newEmail"
-                              type="email"
-                              value={newEmail}
-                              onChange={(e) => setNewEmail(e.target.value)}
-                              placeholder="example@gmail.com"
-                              required
-                          />
-                      </div>
-                      <div className="input-group">
-                          <label htmlFor="newPassword">Password</label>
-                          <input
-                              id="newPassword"
-                              type="password"
-                              value={newPassword}
-                              onChange={(e) => setNewPassword(e.target.value)}
-                              required
-                          />
-                      </div>
-                      <div className="input-group">
-                          <label htmlFor="confirmPassword">Confirm Password</label>
-                          <input
-                              id="confirmPassword"
-                              type="password"
-                              value={confirmPassword}
-                              onChange={(e) => setConfirmPassword(e.target.value)}
-                              required
-                          />
-                      </div>
-                      <button onClick={navigateSignIn} type="submit" className="signup-button">
-                          Create Account
-                      </button>
-                  </form>
-                  <div className="signin-link">
-                      Already have an account?{" "}
-                      <button onClick={navigateSignIn} className="text-button">
-                          Sign In
-                      </button>
-                  </div>
-              </div>
-          </div>
-      </div>
-    </div>
-  );
+        console.log(data);
+
+        if (data.error) {
+            toast({
+                title: "Error",
+                description: data.error,
+                status: "error",
+                duration: 3000,
+                isClosable: true,
+            });
+        } else {
+            navigateSignIn();
+            // It's typically not recommended to store user info in localStorage.
+            // Consider storing only a session token or similar instead.
+            localStorage.setItem("user-info", JSON.stringify(data));
+        }
+
+    } catch (error) {
+        console.error('An error occurred:', error);
+        toast({
+            title: "Error",
+            description: error.toString(),
+            status: "error",
+            duration: 3000,
+            isClosable: true,
+        });
+    }
 }
 
-export default CreateAcctPage;
+    const navigateHome = () => {
+    navigate('/home_page');
+    setShowNavbar(true);
+    }
+
+    useLayoutEffect(() => {
+    setShowNavbar(false);
+    }, []);
+
+    return (
+        <div className="create-account-fullscreen">
+        <div className="create-account-fullscreen">
+            <div className="create-account-side-bar">
+                <img src={logo} alt="logo" height="400px" width="400px" />
+            </div>
+            <div className="create-account-container">
+                <div className="signup-container">
+                    <h2>Create Account</h2>
+                    <form className="signup-form" onSubmit={handleSignUp}>
+                        <div className="input-group">
+                        <label htmlFor="username">Username</label>
+                            <input
+                                id="username"
+                                type="text"
+                                value={inputs.username}
+                                onChange={(e) => setInputs({...inputs, username: e.target.value})}
+                                required
+                            />
+                        </div>
+                        <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                            <input
+                                id="email"
+                                type="email"
+                                value={inputs.email}
+                                onChange={(e) => setInputs({...inputs, email: e.target.value})}
+                                placeholder="example@gmail.com"
+                                required
+                            />
+                        </div>
+                        <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                            <input 
+                                id="password"
+                                type={showPassword ? "text" : "password"}
+                                value={inputs.password}
+                                onChange={(e) => setInputs({...inputs, password: e.target.value})}                                required
+                            />
+                        </div>
+                        <div className="input-group">
+                            <label htmlFor="confirmPassword">Confirm Password</label>
+                            <input
+                                id="confirmPassword"
+                                type="password"
+                                value={confirmPassword}
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <button type="submit" className="signup-button">
+                            Create Account
+                        </button>
+                    </form>
+                    <div className="signin-link">
+                        Already have an account?{" "}
+                        <button onClick={navigateSignIn} className="text-button">
+                            Sign In
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+    }
+
+    export default CreateAcctPage;
--- src/pages/signin_page.jsx
+++ src/pages/signin_page.jsx
@@ -1,89 +1,84 @@
 import React, { useState, useLayoutEffect } from 'react';
 import { useNavigate } from "react-router-dom";
+import { useToast } from "@chakra-ui/react";
 import { useUser } from '../../src/UserContext';
 import './signin_page.css'; 
 import logo from '../assets/CineShare Logo Request.webp';
 
 function SigninPage({ setShowNavbar }) {
     const navigate = useNavigate();
+    const toast = useToast();
+    const userContext = useUser();
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
 
-    
-    const userContext = useUser();
+    useLayoutEffect(() => {
+        setShowNavbar(false);
+    }, []);
 
     const navigateHome = () => {
         navigate('/home_page');
         setShowNavbar(true);
-    }
+    };
 
     const navigateCreateAccount = () => {
         navigate('/createacct_page');
         setShowNavbar(true);
-    }
-
-    const getUsernameFromEmail = (email) => {
-        const userMapping = {
-            'austin@gmail.com': 'Austin Gan',
-            'yco@gmail.com': 'Yco Santos',
-            'philipp@gmail.com': 'Philipp Matthew Suarez',
-            'javi@gmail.com': 'Javi del Rosario',
-            'moist@gmail.com': 'Charles White',
-            'mutahar@gmail.com': 'Mutahar Anas',
-        };
-    
-        return userMapping[email] || ''; 
     };
 
-
-    
-        const handleLogin = (event) => {
-            event.preventDefault();
-            console.log('Login with:', email, password);
-            const activeusername = getUsernameFromEmail(email);
+    const handleLogin = async (event) => {
+        event.preventDefault();
         
+        try {
+            const res = await fetch("/api/users/signin", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ email, password })
+            });
 
-            const isPasswordCorrect = checkPassword(email, password);
-        
-            if (isPasswordCorrect) {
-                console.log('Username:', activeusername);
-                userContext.updateUser(activeusername);
-                console.log('User Context after login:', userContext);
-                navigateHome();
-            } else {
-
-                setErrorMessage('Incorrect email or password');
+            if (!res.ok) {
+                throw new Error('Server responded with a status: ' + res.status);
             }
-        };
-      
 
-        const checkPassword = (email, enteredPassword) => {
-            const userPasswordMapping = {
-                'austin@gmail.com': 'austinpassword',
-                'yco@gmail.com': 'ycopassword',
-                'philipp@gmail.com': 'philipppassword',
-                'javi@gmail.com': 'javipassword',
-                'moist@gmail.com': 'moistpassword',
-                'mutahar@gmail.com': 'mutaharpassword',
-            };
-        
-            return userPasswordMapping[email] === enteredPassword;
-        };
+            const data = await res.json();
 
-    
-    useLayoutEffect(() => {
-        setShowNavbar(false);
-    }, [])
+            if (data.error) {
+                toast({
+                    title: "Error",
+                    description: data.error,
+                    status: "error",
+                    duration: 3000,
+                    isClosable: true,
+                });
+            } else {
+                userContext.updateUser(data.username);
+                navigateHome();
+                localStorage.setItem("user-info", JSON.stringify(data));
+            }
+
+        } catch (error) {
+            console.error('An error occurred:', error);
+            toast({
+                title: "Error",
+                description: error.toString(),
+                status: "error",
+                duration: 3000,
+                isClosable: true,
+            });
+        }
+    };
 
     return (
         <div className="signin-fullscreen">
             <div className="signin-side-bar">
-                <img src = {logo} alt="logo" height = "400px" width = '400px'/>
+                <img src={logo} alt="logo" height="400px" width="400px" />
             </div>
             <div className="signin-container">
                 <div className="login-container">
-                    <h2>Sign in</h2>
+                    <h2>Sign In</h2>
                     <form className="login-form" onSubmit={handleLogin}>
                         <div className="input-group">
                             <label htmlFor="email">Email</label>
@@ -92,7 +87,8 @@ function SigninPage({ setShowNavbar }) {
                                 type="email"
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
-                                placeholder="example.email@gmail.com"
+                                required
+                                placeholder="example@gmail.com"
                             />
                         </div>
                         <div className="input-group">
@@ -102,15 +98,16 @@ function SigninPage({ setShowNavbar }) {
                                 type="password"
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
+                                required
                             />
-                            {errorMessage && <p style={{ color: 'red', marginTop: '8px' }}>{errorMessage}</p>}
                         </div>
-                        <div className="remember-forgot">
-                        </div>
-                        <button type="submit" className="signin-button">Sign in</button>
+                        <button onClick= {navigateHome} type="submit" className="signin-button">Sign In</button>
                     </form>
                     <div className="signup-link">
-                        No account yet? <button onClick={navigateCreateAccount} className="text-button">Sign Up</button>
+                        No account yet?{" "}
+                        <button onClick={navigateCreateAccount} className="text-button">
+                            Sign Up
+                        </button>
                     </div>
                 </div>
             </div>
